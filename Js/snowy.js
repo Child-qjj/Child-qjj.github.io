21a52626558d98a555c347172dd832561736cece
Child-qjj <429814031@qq.com>
2020-07-02 17:04:33 +0800

    todolist

---
+++ Js/snowy.js
@@ -0,0 +1,39 @@
+function init(){
+    //❄
+    let canvas=document.querySelector("#show");
+     ctx=canvas.getContext("2d");
+    var timer=setInterval(snowy,1000);
+ }
+//  init();
+ function snowy() {
+    let content=document.getElementById("container");
+    let div=document.createElement("div");
+    let opc=Math.random();
+    let winWidth = content.offsetWidth;
+    // let winHeight = content.offsetHeight;
+    let top=-50;
+    let size=Math.floor(Math.random()*75+1);
+    let left=Math.random()*(winWidth-size)+Math.random();
+    div.id="snow";
+    div.innerHTML="❄";
+    div.className="tag";
+    div.style="opacity:"+opc+";"+"left:"+left+"px;"+"top:"+top+"px;"+"font-size:"+size+"px;";
+    content.appendChild(div);
+    move(div);
+ }
+     function move(el) {
+         var timer;
+                timer = setInterval(function() {
+					let top = el.offsetTop;
+					let left = el.offsetLeft;
+					let winHeight = window.innerHeight;
+                    let oBox = document.getElementById("container");
+					//清除img 要不会占用内存 增加浏览器的负担 会崩溃的
+					if (top > winHeight || left <0) {
+						clearInterval(timer);
+						oBox.removeChild(el);
+					}
+					el.style.top = (top + 20) + "px";
+					el.style.left = (left - 10) + "px";
+				},100)
+     }
